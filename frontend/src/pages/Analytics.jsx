717dbe70bbb8d82ceaf6a1e2df026e07f8f64bda
Kushagra Bhargava <kushagra2112004@gmail.com>
2026-07-05 11:12:59 +0530

    frontend: added sector impact table

--- frontend/src/pages/Analytics.jsx
+++ frontend/src/pages/Analytics.jsx
@@ -1,14 +1,17 @@
 import SentimentChart from "../components/SentimentChart.jsx";
-import { getMockSentimentTimeseries } from "../data/mockAnalytics.js";
+import SectorImpactTable from "../components/SectorImpactTable.jsx";
+import { getMockSentimentTimeseries, getMockSectorImpact } from "../data/mockAnalytics.js";
 
 export default function Analytics() {
   const { points } = getMockSentimentTimeseries();
+  const { sectors } = getMockSectorImpact();
 
   return (
-    <div className="p-8">
+    <div className="flex flex-col gap-5 p-8">
       <div className="max-w-2xl">
         <SentimentChart points={points} />
       </div>
+      <SectorImpactTable sectors={sectors} />
     </div>
   );
 }
